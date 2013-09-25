22b8b6011af4ebe0839f75bcfc7513e620424d29
WileESpaghetti <dosnlinux@gmail.com>
2013-09-24 20:34:11 -0500

    added skeleton class files

--- orthography/yupik_char.py
+++ orthography/yupik_char.py
@@ -5,3 +5,16 @@
 class YupikChar(object):
     def __init__(self, c):
         self.ychar = c
+
+    def __copy__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __eq__(self):
+        pass
+
--- orthography/yupik_letter.py
+++ orthography/yupik_letter.py
@@ -4,5 +4,62 @@ import orthography
 
 
 class YupikLetter(orthography.YupikChar):
+    """
+    add pronounciation info and alphebetical context to YupikChars
+    """
+
     def __init__(self, c):
         super(YupikLetter, self).__init__(c)
+
+    def is_vowel(self):
+        pass
+
+    def is_prime_vowel(self):
+        pass
+
+    def is_consonant(self):
+        pass
+
+    def is_symbol(self):
+        pass
+
+    def is_valid(self):
+        pass
+
+    ### Manner of articulation ###
+
+    def is_stop(self):
+        pass
+
+    def is_fricative(self):
+        pass
+
+    def is_nasal(self):
+        pass
+
+    ### Place of articulation ###
+
+    def is_labial(self):
+        pass
+
+    def is_velar(self):
+        pass
+
+    def is_apical(self):
+        pass
+
+    ### Pronounciation ###
+
+    def is_voiced(self):
+        pass
+
+    def has_gemination(self):
+        pass
+
+    # XXX: this might be useful for syllables too
+    def has_rhythmic_length(self):
+        pass
+
+    # XXX: this might be useful for syllables too
+    def has_stress(self):
+       pass
