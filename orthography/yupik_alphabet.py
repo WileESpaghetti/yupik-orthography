60d0950316d078a4ac665402d5f4f516c23e9f08
WileESpaghetti <dosnlinux@gmail.com>
2013-09-28 16:40:57 -0500

    added the main letters of the Yup'ik alphabet

--- orthography/yupik_alphabet.py
+++ orthography/yupik_alphabet.py
@@ -1,7 +1,27 @@
 #!python
 #encoding utf-8
-
+from orthography import YupikChar
 
 class YupikAlphabet(object):
     def __init__(self):
-        pass
+        # the 18 official letters
+        self.__alphabet = {
+            u'a': YupikChar(u'a'),
+            u'c': YupikChar(u'c'),
+            u'e': YupikChar(u'e'),
+            u'g': YupikChar(u'g'),
+            u'i': YupikChar(u'i'),
+            u'k': YupikChar(u'k'),
+            u'l': YupikChar(u'l'),
+            u'm': YupikChar(u'm'),
+            u'n': YupikChar(u'n'),
+            u'p': YupikChar(u'p'),
+            u'q': YupikChar(u'q'),
+            u'r': YupikChar(u'r'),
+            u's': YupikChar(u's'),
+            u't': YupikChar(u't'),
+            u'u': YupikChar(u'u'),
+            u'v': YupikChar(u'v'),
+            u'w': YupikChar(u'w'),
+            u'y': YupikChar(u'y'),
+        }
